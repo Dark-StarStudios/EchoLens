1233c9f258e3872365e911693f0ee35b11d88b9a
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2024-01-31 12:53:38 +0000

    v3

---
+++ js/BurgerMenu.js
@@ -0,0 +1,20 @@
+const menu_button = document.getElementById("burger-menu");
+const menu_panel = document.getElementById("burger-menu-panel");
+const x = window.matchMedia("(min-width: 1260px)")
+let key = false;
+
+x.addEventListener("change", function() {
+    if(x.matches){
+        menu_panel.style.display = "none";
+    }
+  }); 
+function Panel(){
+    if(key){
+        menu_panel.style.display = "none";
+        return key = false;
+    }else{
+        menu_panel.style.display = "grid";
+        return key = true;
+    }
+}
+menu_button.addEventListener("click",Panel);
